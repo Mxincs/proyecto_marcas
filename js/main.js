6e809d541c8b0ad27433d6f93f92719fe4c523cd
mxincs <martinbotero1214@gmail.com>
2025-04-12 19:23:50 +0200

    marcas

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
+//Activa mi plugin de scrolltrigger
 gsap.registerPlugin(ScrollTrigger);
 
 // Animación con ScrollTrigger
 gsap.from(".logo", {
+  y: "50vh",
+  scale: 10,
+  yPercent: -50,
   scrollTrigger: {
     trigger: ".contenido", // Esto indica que la animación comienza cuando ".contenido" entra en vista
     start: "top bottom", // Cuando la parte superior de ".contenido" alcanza la parte inferior de la ventana
-    end: "top center", // La animación termina cuando la parte superior de ".contenido" llega al centro de la ventana
+    end: "top 1%", // La animación termina cuando la parte superior de ".contenido" llega al centro de la ventana
     scrub: true, // Hace que la animación se controle de acuerdo con el desplazamiento del scroll
   },
   y: "50vh", // Mueve el logo 50vh hacia abajo desde su posición original
-  scale: 6, // Aumenta el tamaño del logo
-  yPercent: -50, // Desplazamiento adicional en el eje Y
+  scale: 5, // Aumenta el tamaño del logo
+  yPercent: -10, // Desplazamiento adicional en el eje Y
 });
