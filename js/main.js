ba4637e601d35e3c3705229b5f35143776838557
mxincs <martinbotero1214@gmail.com>
2025-04-12 13:54:11 +0200

    marcas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-gsap.to(".cuadrado", {
-  x: 700,
-  duration: 3
-})
+gsap.registerPlugin(ScrollTrigger);
+
+// Animación con ScrollTrigger
+gsap.from(".logo", {
+  scrollTrigger: {
+    trigger: ".contenido", // Esto indica que la animación comienza cuando ".contenido" entra en vista
+    start: "top bottom", // Cuando la parte superior de ".contenido" alcanza la parte inferior de la ventana
+    end: "top center", // La animación termina cuando la parte superior de ".contenido" llega al centro de la ventana
+    scrub: true, // Hace que la animación se controle de acuerdo con el desplazamiento del scroll
+  },
+  y: "50vh", // Mueve el logo 50vh hacia abajo desde su posición original
+  scale: 6, // Aumenta el tamaño del logo
+  yPercent: -50, // Desplazamiento adicional en el eje Y
+});
