366767df7e5e3f9540cb1f40c9bc468b288aaa37
mxincs <martinbotero1214@gmail.com>
2025-05-07 13:36:13 +0200

    marcas

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ gsap.from(".logo", {
   scale: 10,
   yPercent: -50,
   scrollTrigger: {
-    trigger: ".contenido", // Esto indica que la animación comienza cuando ".contenido" entra en vista
+    trigger: ".descripcion", // Esto indica que la animación comienza cuando ".contenido" entra en vista
     start: "top bottom", // Cuando la parte superior de ".contenido" alcanza la parte inferior de la ventana
-    end: "top 1%", // La animación termina cuando la parte superior de ".contenido" llega al centro de la ventana
+    end: "top 300px", // La animación termina cuando la parte superior de ".contenido" llega al centro de la ventana
     scrub: true, // Hace que la animación se controle de acuerdo con el desplazamiento del scroll
   },
   y: "50vh", // Mueve el logo 50vh hacia abajo desde su posición original
-  scale: 5, // Aumenta el tamaño del logo
+  scale: 3.5, // Aumenta el tamaño del logo
   yPercent: -10, // Desplazamiento adicional en el eje Y
 });
